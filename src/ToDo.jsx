ec58fe244bcc02c92a96a78a95731ed8ec15363e
taeheon9625 <xogjs9625@naver,com>
2024-01-30 10:28:05 +0900

    ToDo

---
+++ src/ToDo.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+class ToDo extends React.Component{
+    render(){
+        return(
+            <div className="ToDo">
+                <input type="checkbox" id="todo0" name="todo0" value="todo0"/>
+                <label for="todo0">컴포넌트 만들기</label>
+            </div>
+        )
+    }
+}
